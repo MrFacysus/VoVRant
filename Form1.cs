0317f0a00c3d5ade9f8003321192d2a49b8e6dbf
Daniel Jacob <danieljacobowp@gmail.com>
2022-07-01 23:22:38 -0300

    Update Form1.cs
    Code improvements.

--- Form1.cs
+++ Form1.cs
@@ -1,116 +1,121 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VoVRant
 {
     public partial class Form1 : Form
     {
-        [DllImport("User32.dll")]
-        public static extern short GetAsyncKeyState(Keys vKey);
+        private const int SizeX = 32;
+        private const int SizeY = 16;
 
-        public static int SizeX = 32;
-        public static int SizeY = 16;
-        public static int ScreenWidth = Screen.PrimaryScreen.Bounds.Width;
-        public static int ScreenHeight = Screen.PrimaryScreen.Bounds.Height;
-        public int ScreenWidthHalf = ScreenWidth / 2;
-        public int ScreenHeightHalf = ScreenHeight / 2;
-        public int IDX;
-        public Bitmap Visual = new Bitmap(SizeX, SizeY);
-        public bool FoundLeftSide;
-        public bool FoundRightSide;
-        public bool CenterAligned;
-        public bool BreakOut;
-        public List<Point> HitPoints;
-        public double AVGPoint;
+        private static readonly int ScreenWidth = Screen.PrimaryScreen.Bounds.Width;
+        private static readonly int ScreenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+        private readonly int _screenHeightHalf = ScreenHeight / 2;
+        private readonly int _screenWidthHalf = ScreenWidth / 2;
+
+        private readonly Bitmap _visual = new Bitmap(SizeX, SizeY);
+        private double _avgPoint;
+        private bool _breakOut;
+        private bool _centerAligned;
+
+        private bool _foundLeftSide;
+        private bool _foundRightSide;
+
+        private List<Point> _hitPoints;
+        private int _idx;
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(Keys vKey);
+        
+        [DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
+        private static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            Task Aim = new Task(() =>
+            new Thread(Action) { IsBackground = true }.Start();
+        }
+
+        private void Action()
+        {
+            while (true)
             {
-                while (true)
+                if ((GetAsyncKeyState(Keys.XButton2) & 0x8000) == 0)
+                    goto WaitCallback;
+
+                _hitPoints = new List<Point>();
+                _foundLeftSide = false;
+                _centerAligned = false;
+                _foundRightSide = false;
+                _breakOut = false;
+                _avgPoint = 0;
+                _idx = 0;
+
+                using (var graphics = Graphics.FromImage(_visual))
                 {
-                    if (!(GetAsyncKeyState(Keys.XButton2) < 0))
+                    //Take a screenshot
+                    graphics.CopyFromScreen(_screenWidthHalf - SizeX / 2, _screenHeightHalf - SizeY / 2, 0, 0,
+                        _visual.Size);
+
+                    //Get all pixels
+                    for (var i = 0; i < _visual.Width; ++i)
                     {
-                        HitPoints = new List<Point>();
-                        FoundLeftSide = false;
-                        CenterAligned = false;
-                        FoundRightSide = false;
-                        BreakOut = false;
-                        AVGPoint = 0;
-                        IDX = 0;
-                        using (Graphics graphics = Graphics.FromImage(Visual))
+                        for (var j = 0; j < _visual.Height; ++j)
                         {
-                            //Take a screenshot
-                            graphics.CopyFromScreen(ScreenWidthHalf - (SizeX / 2), ScreenHeightHalf - (SizeY / 2), 0, 0, Visual.Size);
-                            //Get all pixels
-                            for (int i = 0; i < Visual.Width; i++)
+                            var centerColor = _visual.GetPixel(i, j);
+                            //If good color, shoot
+                            if (centerColor.R <= 230 || centerColor.B <= 230 || centerColor.G >= 200)
+                                continue;
+
+                            //Check both sides
+                            if (i < _visual.Width / 2)
+                                _foundLeftSide = true;
+                            else if (i > _visual.Width / 2) _foundRightSide = true;
+
+                            //Add it to list
+                            _hitPoints.Add(new Point(i, j));
+
+                            //Check if list avg is head like
+                            foreach (var p in _hitPoints)
                             {
-                                for (int j = 0; j < Visual.Height; j++)
-                                {
-                                    Color CenterColor = Visual.GetPixel(i, j);
-                                    //If good color, shoot
-                                    if ((CenterColor.R > 230) && (CenterColor.B > 230) && (CenterColor.G < 200))
-                                    {
-                                        //Check both sides
-                                        if (i < (Visual.Width / 2))
-                                        {
-                                            FoundLeftSide = true;
-                                        }
-                                        else if (i > (Visual.Width / 2))
-                                        {
-                                            FoundRightSide = true;
-                                        }
-
-                                        //Add it to list
-                                        HitPoints.Add(new Point(i, j));
-
-                                        //Check if list avg is head like
-                                        foreach(Point p in HitPoints)
-                                        {
-                                            IDX++;
-                                            AVGPoint += Math.Sqrt(Math.Pow(p.X - (Visual.Width / 2), 2) + Math.Pow(p.Y - (Visual.Height / 2), 2));
-                                            if (((AVGPoint / IDX) < 12) && (IDX > 4))
-                                            {
-                                                CenterAligned = true;
-                                                break;
-                                            }
-                                        }
-
-                                        //Shoot it boii
-                                        if ((FoundRightSide) && (FoundLeftSide) && (CenterAligned))
-                                        {
-                                            SendKeys.SendWait("k");
-                                            BreakOut = true;
-                                            break;
-                                        }
-                                    }
-                                }
-                                if (BreakOut)
-                                {
-                                    break;
-                                    Thread.Sleep(250);
-                                }
+                                ++_idx;
+                                _avgPoint += Math.Sqrt(Math.Pow(p.X - _visual.Width / 2, 2) +
+                                                       Math.Pow(p.Y - _visual.Height / 2, 2));
+                                if (!(_avgPoint / _idx < 12) || _idx <= 4)
+                                    continue;
+                                _centerAligned = true;
+                                break;
                             }
+
+                            //Shoot it boii
+                            if (!_foundRightSide || !_foundLeftSide || !_centerAligned)
+                                continue;
+
+                            mouse_event(0x02 | 0x04, 0, 0, 0, 0);
+                            
+                            _breakOut = true;
+                            break;
                         }
-                        Thread.Sleep(6);
+
+                        if (!_breakOut)
+                            continue;
+
+                        break;
                     }
                 }
-            });
-            Aim.Start();
+
+                WaitCallback:
+                Thread.Sleep(10);
+            }
         }
     }
 }
